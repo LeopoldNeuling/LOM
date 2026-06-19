33cba94c1ae55561aa1b053e6e5a95d9a0ab2f7f
Leopold Neuling <leopold.neuling@icloud.com>
2026-06-19 03:11:22 +0200

    layout team page for pc done

--- src/pages/UeberMich.jsx
+++ src/pages/UeberMich.jsx
@@ -32,139 +32,16 @@ const mobileLebensLaufFortb = isMobile
   : {};
 const fontSize = "16px";
 
-function Lebenslauf() {
-  function Head({ children }) {
-    return (
-      <Typography color="secondary.main" variant="h5" sx={{ gridRow: 2 }}>
-        Lebenslauf
-        {children}
-      </Typography>
-    );
-  }
-  function Body() {
-    return (
-      <div style={{ fontSize: fontSize, color: "inherit" }}>
-        <ul>
-          <li>
-            2015-2018: Primärqualifizierender Studiengang Ergotherapie (B.Sc.),
-            IB-Hochschule Berlin
-          </li>
-          <li>
-            10/2018 – 06/2021: Ergotherapeutin (B.Sc.) Ambulante Arbeitstherapie
-            am Anderen Ort,
-            <br />
-            St. Hedwig Krankenhaus, Berlin
-          </li>
-          <li>
-            08/2021-04/2023: Ergotherapeutin Berufliches Trainingszentrum im
-            Rot-Kreuz-Institut gGmbH Berlin
-          </li>
-          <li>
-            Seit 04/2023: Klinische Ergotherapeutin Psychiatrische
-            Institutsambulanz (PIA), Stationsäquivalente Behandlung (StäB) und
-            TK-E, Theodor-Wenzel-Werk e.V. Berlin
-          </li>
-          <li>
-            09/2023-03/25: Ausbildung Lösungsorientierte Maltherapie LOM® (May
-            Carro Cabaiero und Cora Egger)
-          </li>
-          <li>
-            03/2026: Gründung Praxis für Ergotherapie und Lösungsorientiertes
-            Malen
-          </li>
-        </ul>
-      </div>
-    );
-  }
-  return (
-    <div style={{ ...mobileLebensLaufFortb }}>
-      {isMobile() ? (
-        <Accordion>
-          <AccordionSummary expandIcon={<ExpandMore />}>
-            <Head />
-          </AccordionSummary>
-          <AccordionDetails>
-            <Body />
-          </AccordionDetails>
-        </Accordion>
-      ) : (
-        <Head>
-          <Body />
-        </Head>
-      )}
-    </div>
-  );
-}
-function Fortbildungen() {
-  function Head({ children }) {
-    return (
-      <Typography color="secondary.main" sx={{ gridRow: 2 }} variant="h5">
-        Fortbildungen
-        {children}
-      </Typography>
-    );
-  }
-
-  // before team_version create two branches: team_version and maria_version then code in team_version and push to main
-  // *** for settings main branch to other branch which is behind - save other branch!! ***
-  // git checkout main
-  // git reset --hard other-branch
-  // git push --force-with-lease origin main
-
-  function Body() {
-    return (
-      <div style={{ fontSize: fontSize, color: "inherit" }}>
-        <ul>
-          <li>
-            Biografisches und kreatives Schreiben in unterschiedlichen
-            Lebensphasen und Lebenskrisen
-          </li>
-          <li>Dialektisch Behaviorale Therapie (Einfühung und Vertiefung)</li>
-          <li>Achtsamkeitstraining MBSR (Mindful Based Stress Reduction)</li>
-
-          <li>Umgang mit Depressionen</li>
-          <li>Umgang mit Angstpatienten</li>
-          <li>Open Dialogue (Zusatzqualifikation)</li>
-          <li>
-            Motivational Interviewing bei Menschen mit substanzbezogenen
-            Störungen
-          </li>
-          <li>Kränkbarkeit und Kränkung bei narzisstischen Persönlichkeiten</li>
-          <li>Scham - die tabuisierte Emotion</li>
-          <li>Das neue Bundesteilhabegesetz</li>
-          <li>Wege in den Beruf nach einer psychischen Erkrankung</li>
-          <li>Basiswissen Psychopharmakologie</li>
-          <li>Rechtsgrundlagen zur Teilhabe am Arbeitsleben</li>
-          <li>Hamet 3</li>
-        </ul>
-      </div>
-    );
-  }
-  return (
-    <div style={{ ...mobileLebensLaufFortb }}>
-      {isMobile() ? (
-        <Accordion>
-          <AccordionSummary expandIcon={<ExpandMore />}>
-            <Head />
-          </AccordionSummary>
-          <AccordionDetails>
-            <Body />
-          </AccordionDetails>
-        </Accordion>
-      ) : (
-        <Head>
-          <Body />
-        </Head>
-      )}
-    </div>
-  );
-}
+// before team_version create two branches: team_version and maria_version then code in team_version and push to main
+// *** for settings main branch to other branch which is behind - save other branch!! ***
+// git checkout main
+// git reset --hard other-branch
+// git push --force-with-lease origin main
 
 export default function UeberMich({ myRef }) {
   const audioRef = useRef(null);
   const [playing, setPlaying] = useState(false);
   const [progress, setProgress] = useState(0);
-  const [muted, setMuted] = useState(false);
 
   const playAudio = () => {
     audioRef.current.play();
@@ -178,7 +55,6 @@ export default function UeberMich({ myRef }) {
     pauseAudio();
     audioRef.current.currentTime = 0;
   };
-  const toggleMute = () => setMuted((prev) => !prev);
   const getAudioProgress = () => {
     const audio = audioRef.current;
     const percent = (audio.currentTime / audio.duration) * 100;
@@ -190,11 +66,6 @@ export default function UeberMich({ myRef }) {
     audio.addEventListener("ended", toStartAudio);
     return () => audio.removeEventListener("ended", toStartAudio);
   }, []);
-  useEffect(() => {
-    audioRef.current.muted = muted;
-  }, [muted]);
-
-  const pageStyle = isMobile() ? {} : { maxHeight: "600px" };
 
   return (
     <Box
@@ -204,7 +75,7 @@ export default function UeberMich({ myRef }) {
         isMobile() ? "mobile-page mobile-content-page" : "page content-page"
       }
       sx={{
-        ...pageStyle,
+        maxHeight: "1000px",
 
         display: isMobile() ? "flex" : "grid",
         flexDirection: "column",
@@ -212,19 +83,21 @@ export default function UeberMich({ myRef }) {
         alignItems: "center",
 
         placeItems: "center",
-        gridTemplateColumns: "1fr 1fr 1fr",
-        gridTemplateRows: "1fr 5fr",
+        gridTemplateColumns: "1fr 1fr",
+        gridTemplateRows: "1fr 1fr 5fr",
+        gap: "50px",
       }}
     >
-      {!isMobile() && <Lebenslauf />}
+      <Typography
+        color="secondary.main"
+        variant="h2"
+        sx={{ gridColumn: "span 2" }}
+      >
+        Unser Team
+      </Typography>
+
+      {/* Maria */}
       <div>
-        <Typography
-          color="secondary.main"
-          variant="h2"
-          sx={{ gridArea: "1/2" }}
-        >
-          Über mich
-        </Typography>
         <Card
           sx={{
             display: "flex",
@@ -234,20 +107,7 @@ export default function UeberMich({ myRef }) {
         >
           <Box sx={{ display: "flex", flexDirection: "column" }}>
             <CardContent sx={{ flex: "1 0 auto" }}>
-              <Typography component="div" variant="h5">
-                <sub>
-                  <FormatQuote sx={{ fontSize: "15px" }} />
-                </sub>
-                Ich über mich
-                <sup>
-                  <FormatQuote sx={{ rotate: "180deg", fontSize: "15px" }} />
-                </sup>
-              </Typography>
-              <Typography
-                variant="subtitle1"
-                component="div"
-                sx={{ color: "text.secondary" }}
-              >
+              <Typography variant="h5" sx={{ color: "text.secondary" }}>
                 Maria Neuling
               </Typography>
             </CardContent>
@@ -281,14 +141,63 @@ export default function UeberMich({ myRef }) {
                 )}
               </IconButton>
 
+              <audio
+                src={introAudio}
+                ref={audioRef}
+                onTimeUpdate={getAudioProgress}
+              />
+            </Box>
+          </Box>
+          <CardMedia
+            component="img"
+            sx={{ width: 150, height: 200 }}
+            image={fotoMama}
+          />
+        </Card>
+      </div>
+
+      {/* Other */}
+      <div>
+        <Card
+          sx={{
+            display: "flex",
+            gridRow: 2,
+            marginTop: isMobile() ? "5vh" : 0,
+          }}
+        >
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <CardContent sx={{ flex: "1 0 auto" }}>
+              <Typography variant="h5" sx={{ color: "text.secondary" }}>
+                Max Musterman
+              </Typography>
+            </CardContent>
+            <LinearProgress
+              variant="determinate"
+              value={progress}
+              sx={{ marginInline: "2vh" }}
+              color="info"
+            />
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <IconButton onClick={toStartAudio}>
+                <SkipPrevious fontSize="large" />
+              </IconButton>
+
               <IconButton
-                onClick={toggleMute}
-                color={muted ? "default" : "info"}
+                color={playing ? "info" : "default"}
+                onClick={() => {
+                  playing ? pauseAudio() : playAudio();
+                }}
               >
-                {muted ? (
-                  <VolumeOff fontSize="large" />
+                {playing ? (
+                  <Pause fontSize="large" />
                 ) : (
-                  <VolumeUp fontSize="large" />
+                  <PlayArrow fontSize="large" />
                 )}
               </IconButton>
 
@@ -299,12 +208,173 @@ export default function UeberMich({ myRef }) {
               />
             </Box>
           </Box>
-          <CardMedia component="img" sx={{ width: 151 }} image={fotoMama} />
+          <CardMedia
+            component="img"
+            sx={{ width: 150, height: 200 }}
+            image={""}
+          />
         </Card>
       </div>
 
-      {isMobile() && <Lebenslauf />}
-      <Fortbildungen />
+      {/* Info Maria */}
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+          placeItems: "start",
+          // background: "red",
+        }}
+      >
+        <div>
+          <Typography color="secondary.main" variant="h6">
+            Lebenslauf
+          </Typography>
+          <Typography color="secondary.main" variant="body1">
+            <ul>
+              <li>
+                2015-2018: Primärqualifizierender Studiengang Ergotherapie
+                (B.Sc.), IB-Hochschule Berlin
+              </li>
+              <li>
+                10/2018 – 06/2021: Ergotherapeutin (B.Sc.) Ambulante
+                Arbeitstherapie am Anderen Ort, St. Hedwig Krankenhaus, Berlin
+              </li>
+              <li>
+                08/2021-04/2023: Ergotherapeutin Berufliches Trainingszentrum im
+                Rot-Kreuz-Institut gGmbH Berlin
+              </li>
+              <li>
+                Seit 04/2023: Klinische Ergotherapeutin Psychiatrische
+                Institutsambulanz (PIA), Stationsäquivalente Behandlung (StäB)
+                und TK-E, Theodor-Wenzel-Werk e.V. Berlin
+              </li>
+              <li>
+                09/2023-03/25: Ausbildung Lösungsorientierte Maltherapie LOM®
+                (May Carro Cabaiero und Cora Egger)
+              </li>
+              <li>
+                03/2026: Gründung Praxis für Ergotherapie und
+                Lösungsorientiertes Malen
+              </li>
+            </ul>
+          </Typography>
+        </div>
+        <div>
+          <Typography color="secondary.main" variant="h6">
+            Fortbildungen
+          </Typography>
+          <Typography color="secondary.main" variant="body1">
+            <ul>
+              <li>
+                Biografisches und kreatives Schreiben in unterschiedlichen
+                Lebensphasen und Lebenskrisen
+              </li>
+              <li>
+                Dialektisch Behaviorale Therapie (Einfühung und Vertiefung)
+              </li>
+              <li>
+                Achtsamkeitstraining MBSR (Mindful Based Stress Reduction)
+              </li>
+              <li>Umgang mit Depressionen</li>
+              <li>Umgang mit Angstpatienten</li>
+              <li>Open Dialogue (Zusatzqualifikation)</li>
+              <li>
+                Motivational Interviewing bei Menschen mit substanzbezogenen
+                Störungen
+              </li>
+              <li>
+                Kränkbarkeit und Kränkung bei narzisstischen Persönlichkeiten
+              </li>
+              <li>Scham - die tabuisierte Emotion</li>
+              <li>Das neue Bundesteilhabegesetz</li>
+              <li>Wege in den Beruf nach einer psychischen Erkrankung</li>
+              <li>Basiswissen Psychopharmakologie</li>
+              <li>Rechtsgrundlagen zur Teilhabe am Arbeitsleben</li>
+              <li>Hamet 3</li>
+            </ul>
+          </Typography>
+        </div>
+      </div>
+
+      {/* Info Other */}
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+          placeItems: "start",
+          // background: "red",
+        }}
+      >
+        <div>
+          <Typography color="secondary.main" variant="h6">
+            Lebenslauf
+          </Typography>
+          <Typography color="secondary.main" variant="body1">
+            <ul>
+              <li>
+                2015-2018: Primärqualifizierender Studiengang Ergotherapie
+                (B.Sc.), IB-Hochschule Berlin
+              </li>
+              <li>
+                10/2018 – 06/2021: Ergotherapeutin (B.Sc.) Ambulante
+                Arbeitstherapie am Anderen Ort, St. Hedwig Krankenhaus, Berlin
+              </li>
+              <li>
+                08/2021-04/2023: Ergotherapeutin Berufliches Trainingszentrum im
+                Rot-Kreuz-Institut gGmbH Berlin
+              </li>
+              <li>
+                Seit 04/2023: Klinische Ergotherapeutin Psychiatrische
+                Institutsambulanz (PIA), Stationsäquivalente Behandlung (StäB)
+                und TK-E, Theodor-Wenzel-Werk e.V. Berlin
+              </li>
+              <li>
+                09/2023-03/25: Ausbildung Lösungsorientierte Maltherapie LOM®
+                (May Carro Cabaiero und Cora Egger)
+              </li>
+              <li>
+                03/2026: Gründung Praxis für Ergotherapie und
+                Lösungsorientiertes Malen
+              </li>
+            </ul>
+          </Typography>
+        </div>
+        <div>
+          <Typography color="secondary.main" variant="h6">
+            Fortbildungen
+          </Typography>
+          <Typography color="secondary.main" variant="body1">
+            <ul>
+              <li>
+                Biografisches und kreatives Schreiben in unterschiedlichen
+                Lebensphasen und Lebenskrisen
+              </li>
+              <li>
+                Dialektisch Behaviorale Therapie (Einfühung und Vertiefung)
+              </li>
+              <li>
+                Achtsamkeitstraining MBSR (Mindful Based Stress Reduction)
+              </li>
+              <li>Umgang mit Depressionen</li>
+              <li>Umgang mit Angstpatienten</li>
+              <li>Open Dialogue (Zusatzqualifikation)</li>
+              <li>
+                Motivational Interviewing bei Menschen mit substanzbezogenen
+                Störungen
+              </li>
+              <li>
+                Kränkbarkeit und Kränkung bei narzisstischen Persönlichkeiten
+              </li>
+              <li>Scham - die tabuisierte Emotion</li>
+              <li>Das neue Bundesteilhabegesetz</li>
+              <li>Wege in den Beruf nach einer psychischen Erkrankung</li>
+              <li>Basiswissen Psychopharmakologie</li>
+              <li>Rechtsgrundlagen zur Teilhabe am Arbeitsleben</li>
+              <li>Hamet 3</li>
+            </ul>
+          </Typography>
+        </div>
+      </div>
     </Box>
   );
 }
