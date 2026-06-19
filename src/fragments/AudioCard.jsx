c1adcf4f622e6891ba4cf297477971aaac4fd9b4
Leopold Neuling <leopold.neuling@icloud.com>
2026-06-19 03:35:05 +0200

    standalone audio component

---
+++ src/fragments/AudioCard.jsx
@@ -0,0 +1,103 @@
+import { useRef, useEffect, useState } from "react";
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  LinearProgress,
+  IconButton,
+  CardMedia,
+} from "@mui/material";
+import { SkipPrevious, Pause, PlayArrow } from "@mui/icons-material";
+import { isMobile } from "../helper/helperFunc";
+
+export default function AudioCard({ name, voiceline, photo }) {
+  const audioRef = useRef(null);
+  const [playing, setPlaying] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  const playAudio = () => {
+    audioRef.current.play();
+    setPlaying(true);
+  };
+  const pauseAudio = () => {
+    audioRef.current.pause();
+    setPlaying(false);
+  };
+  const toStartAudio = () => {
+    pauseAudio();
+    audioRef.current.currentTime = 0;
+  };
+  const getAudioProgress = () => {
+    const audio = audioRef.current;
+    const percent = (audio.currentTime / audio.duration) * 100;
+    setProgress(percent);
+  };
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    audio.addEventListener("ended", toStartAudio);
+    return () => audio.removeEventListener("ended", toStartAudio);
+  }, []);
+
+  return (
+    <div>
+      <Card
+        sx={{
+          display: "flex",
+          gridRow: 2,
+          marginTop: isMobile() ? "5vh" : 0,
+        }}
+      >
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <CardContent sx={{ flex: "1 0 auto" }}>
+            <Typography variant="h5" sx={{ color: "text.secondary" }}>
+              Maria Neuling
+            </Typography>
+          </CardContent>
+          <LinearProgress
+            variant="determinate"
+            value={progress}
+            sx={{ marginInline: "2vh" }}
+            color="info"
+          />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <IconButton onClick={toStartAudio}>
+              <SkipPrevious fontSize="large" />
+            </IconButton>
+
+            <IconButton
+              color={playing ? "info" : "default"}
+              onClick={() => {
+                playing ? pauseAudio() : playAudio();
+              }}
+            >
+              {playing ? (
+                <Pause fontSize="large" />
+              ) : (
+                <PlayArrow fontSize="large" />
+              )}
+            </IconButton>
+
+            <audio
+              src={voiceline}
+              ref={audioRef}
+              onTimeUpdate={getAudioProgress}
+            />
+          </Box>
+        </Box>
+        <CardMedia
+          component="img"
+          sx={{ width: 150, height: 200 }}
+          image={photo}
+        />
+      </Card>
+    </div>
+  );
+}
--- src/pages/UeberMich.jsx
+++ src/pages/UeberMich.jsx
@@ -23,6 +23,7 @@ import {
 import fotoMama from "../assets/fotoMama2.jpg";
 import introAudio from "../assets/audios/intro.m4a";
 import { isMobile } from "../helper/helperFunc";
+import AudioCard from "../fragments/AudioCard";
 
 const mobileLebensLaufFortb = isMobile
   ? {
@@ -39,34 +40,6 @@ const fontSize = "16px";
 // git push --force-with-lease origin main
 
 export default function UeberMich({ myRef }) {
-  const audioRef = useRef(null);
-  const [playing, setPlaying] = useState(false);
-  const [progress, setProgress] = useState(0);
-
-  const playAudio = () => {
-    audioRef.current.play();
-    setPlaying(true);
-  };
-  const pauseAudio = () => {
-    audioRef.current.pause();
-    setPlaying(false);
-  };
-  const toStartAudio = () => {
-    pauseAudio();
-    audioRef.current.currentTime = 0;
-  };
-  const getAudioProgress = () => {
-    const audio = audioRef.current;
-    const percent = (audio.currentTime / audio.duration) * 100;
-    setProgress(percent);
-  };
-
-  useEffect(() => {
-    const audio = audioRef.current;
-    audio.addEventListener("ended", toStartAudio);
-    return () => audio.removeEventListener("ended", toStartAudio);
-  }, []);
-
   return (
     <Box
       ref={myRef}
@@ -97,124 +70,10 @@ export default function UeberMich({ myRef }) {
       </Typography>
 
       {/* Maria */}
-      <div>
-        <Card
-          sx={{
-            display: "flex",
-            gridRow: 2,
-            marginTop: isMobile() ? "5vh" : 0,
-          }}
-        >
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
-            <CardContent sx={{ flex: "1 0 auto" }}>
-              <Typography variant="h5" sx={{ color: "text.secondary" }}>
-                Maria Neuling
-              </Typography>
-            </CardContent>
-            <LinearProgress
-              variant="determinate"
-              value={progress}
-              sx={{ marginInline: "2vh" }}
-              color="info"
-            />
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-evenly",
-              }}
-            >
-              <IconButton onClick={toStartAudio}>
-                <SkipPrevious fontSize="large" />
-              </IconButton>
-
-              <IconButton
-                color={playing ? "info" : "default"}
-                onClick={() => {
-                  playing ? pauseAudio() : playAudio();
-                }}
-              >
-                {playing ? (
-                  <Pause fontSize="large" />
-                ) : (
-                  <PlayArrow fontSize="large" />
-                )}
-              </IconButton>
-
-              <audio
-                src={introAudio}
-                ref={audioRef}
-                onTimeUpdate={getAudioProgress}
-              />
-            </Box>
-          </Box>
-          <CardMedia
-            component="img"
-            sx={{ width: 150, height: 200 }}
-            image={fotoMama}
-          />
-        </Card>
-      </div>
+      <AudioCard name="Maria Neuling" voiceline={introAudio} photo={fotoMama} />
 
       {/* Other */}
-      <div>
-        <Card
-          sx={{
-            display: "flex",
-            gridRow: 2,
-            marginTop: isMobile() ? "5vh" : 0,
-          }}
-        >
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
-            <CardContent sx={{ flex: "1 0 auto" }}>
-              <Typography variant="h5" sx={{ color: "text.secondary" }}>
-                Max Musterman
-              </Typography>
-            </CardContent>
-            <LinearProgress
-              variant="determinate"
-              value={progress}
-              sx={{ marginInline: "2vh" }}
-              color="info"
-            />
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-evenly",
-              }}
-            >
-              <IconButton onClick={toStartAudio}>
-                <SkipPrevious fontSize="large" />
-              </IconButton>
-
-              <IconButton
-                color={playing ? "info" : "default"}
-                onClick={() => {
-                  playing ? pauseAudio() : playAudio();
-                }}
-              >
-                {playing ? (
-                  <Pause fontSize="large" />
-                ) : (
-                  <PlayArrow fontSize="large" />
-                )}
-              </IconButton>
-
-              <audio
-                src={introAudio}
-                ref={audioRef}
-                onTimeUpdate={getAudioProgress}
-              />
-            </Box>
-          </Box>
-          <CardMedia
-            component="img"
-            sx={{ width: 150, height: 200 }}
-            image={""}
-          />
-        </Card>
-      </div>
+      <AudioCard name="Maria Neuling" voiceline={introAudio} photo={""} />
 
       {/* Info Maria */}
       <div
