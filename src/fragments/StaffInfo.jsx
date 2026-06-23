20d3ff6938d73e1475fb01e166ae6d6d10424fe3
Leopold Neuling <leopold.neuling@icloud.com>
2026-06-24 01:18:12 +0200

    bold set dates automaticall<

--- src/fragments/StaffInfo.jsx
+++ src/fragments/StaffInfo.jsx
@@ -3,79 +3,65 @@ import {
   AccordionDetails,
   AccordionSummary,
   Typography,
+  Stack,
+  Button,
 } from "@mui/material";
 import { ExpandMore } from "@mui/icons-material";
 import { isMobile } from "../helper/helperFunc";
+import { Children, cloneElement, useState } from "react";
 
 const accodionStyling = { marginTop: "2vw" };
-
-function Lebenslauf({ content }) {
-  return isMobile() ? (
-    <Accordion sx={{ ...accodionStyling }}>
-      <AccordionSummary expandIcon={<ExpandMore />}>
-        <Typography color="secondary.main" variant="h6">
-          Lebenslauf
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Typography color="secondary.main" variant="body1" component="div">
-          {content}
-        </Typography>
-      </AccordionDetails>
-    </Accordion>
-  ) : (
-    <>
-      <Typography color="secondary.main" variant="h6">
-        Lebenslauf
-      </Typography>
-      <Typography color="secondary.main" variant="body1" component="div">
-        {content}
-      </Typography>
-    </>
-  );
-}
-
-function Fortbildungen({ content }) {
-  return isMobile() ? (
-    <Accordion sx={{ ...accodionStyling }}>
-      <AccordionSummary expandIcon={<ExpandMore />}>
-        <Typography color="secondary.main" variant="h6">
-          Fortbildungen
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Typography color="secondary.main" variant="body1" component="div">
-          {content}
-        </Typography>
-      </AccordionDetails>
-    </Accordion>
-  ) : (
-    <>
-      <Typography color="secondary.main" variant="h6">
-        Fortbildungen
-      </Typography>
-      <Typography color="secondary.main" variant="body1" component="div">
-        {content}
-      </Typography>
-    </>
-  );
-}
 
 export default function StaffInfo({ children, row, col }) {
   const [lebenslauf, fortbildungen] = children;
 
+  const formattedLebenslauf = cloneElement(lebenslauf, {
+    children: Children.map(lebenslauf.props.children, (child) => {
+      const curText = child.props.children;
+
+      if (typeof curText !== "string" || !curText.includes(":")) {
+        return child;
+      }
+
+      const colonPos = curText.indexOf(":");
+      const date = curText.substring(0, colonPos);
+      const rest = curText.substring(colonPos + 1);
+
+      return cloneElement(child, {
+        children: (
+          <>
+            <b>{date}:</b>
+            {rest}
+          </>
+        ),
+      });
+    }),
+  });
+
+  const [vLebenslauf, setV] = useState(true);
+
+  const navMargin = isMobile() ? { marginTop: "2vh" } : {};
+
   return (
-    <div
-      style={{
-        display: isMobile() ? "flex" : "grid",
-        placeItems: "start",
-        flexDirection: "column",
-        gridColumn: col,
-        gridRow: row,
-      }}
+    <Stack
+      spacing={{ xs: 1, sm: 2 }}
+      direction="row"
+      useFlexGap
+      sx={{ flexWrap: "wrap", ...navMargin }}
     >
-      <Lebenslauf content={lebenslauf} />
-      <Fortbildungen content={fortbildungen} />
-    </div>
+      <Button
+        onClick={() => setV(true)}
+        variant={vLebenslauf ? "contained" : "outlined"}
+      >
+        Lebenslauf
+      </Button>
+      <Button
+        onClick={() => setV(false)}
+        variant={!vLebenslauf ? "contained" : "outlined"}
+      >
+        Fortbildungen
+      </Button>
+      {vLebenslauf ? formattedLebenslauf : fortbildungen}
+    </Stack>
   );
 }
